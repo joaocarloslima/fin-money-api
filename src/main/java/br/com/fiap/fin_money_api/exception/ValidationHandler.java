6304820bef3ecb005a0ed18e00dcc10759940de8
joaocarloslima <joaocarloslima@me.com>
2025-04-04 10:19:50 -0300

    Refactor validation error handling to use constructor for creating ValidationError instances

--- src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
+++ src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
@@ -3,6 +3,7 @@ package br.com.fiap.fin_money_api.exception;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ValidationHandler {
 
-    record ValidationError (String field, String message){}
+    record ValidationError (String field, String message){
+
+        public ValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }}
 
     @ExceptionHandler(exception = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public List<ValidationError> handler(MethodArgumentNotValidException e){
         return e.getFieldErrors()
                 .stream()
-                .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
+                .map(ValidationError::new)
                 .toList();
-    }
+        }   
     
 }
