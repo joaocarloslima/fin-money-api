02261246c7f6e1bbd1061c4e09ba75da18c83e67
joaocarloslima <joaocarloslima@me.com>
2025-05-09 16:04:26 -0300

    Implement JWT authentication

--- src/main/java/br/com/fiap/fin_money_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/fin_money_api/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package br.com.fiap.fin_money_api.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,18 +15,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthFilter authFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests(
             auth -> auth
                 //.requestMatchers("/categories/**").hasRole("ADMIN")
+                .requestMatchers("/login/**").permitAll()
                 .anyRequest().authenticated()
         )
         .csrf(csrf -> csrf.disable())
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
         .httpBasic(Customizer.withDefaults())
         .build();
     }
@@ -50,6 +59,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 
 }
